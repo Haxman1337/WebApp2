41d04eceb13b69274259d38751fe5e0488388209
Daniil Haxman <smartdan112233@gmail.com>
2021-02-10 11:30:22 +0300

    Message

--- WebApp2/Controllers/HomeController.cs
+++ WebApp2/Controllers/HomeController.cs
@@ -5,11 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 using WebApp2.Models;
 using WebApp2.Tools;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApp2.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             ViewModel vm = new ViewModel();
@@ -18,6 +20,17 @@ namespace WebApp2.Controllers
             vm.Orders = new List<Order>();
             vm.Orders.AddRange(XmlWorks3.Orders);
             return View(vm);
+        }
+
+        public ActionResult Create([Bind(Include = "Id,Name,Author,Year")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                //create user
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
         }
 
         public ActionResult About()
--- WebApp2/Models/Customer.cs
+++ WebApp2/Models/Customer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,11 +20,54 @@ namespace WebApp2.Models
             Email = email;
             Phone = phone;
         }
+        /*
+        [Required(ErrorMessage = "Не указан id")]
+        //[RegularExpression(@"^\d+$", ErrorMessage = "Некорректный id")]
         public string Id { get; set; }
+        [Required(ErrorMessage = "Не указано имя")]
+        //[RegularExpression(@"^[A-Za-zFА-Яа-я]+$", ErrorMessage = "Некорректное имя")]
+        //[StringLength(50, MinimumLength = 0, ErrorMessage = "Длина строки не может быть более 50 символов")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Не указана дата рождения")]
+        //[RegularExpression(@"\d{2}[.]\d{2}[.]\d{4}", ErrorMessage = "Некорректная дата")]
         public string Birdate { get; set; }
+        [Required(ErrorMessage = "Не указана дата регистрации")]
+       // [RegularExpression(@"\d{2}[.]\d{2}[.]\d{4}", ErrorMessage = "Некорректная дата")]
         public string Regdate { get; set; }
+        [Required(ErrorMessage = "Не указана почта")]
+      //  [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес почты")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Не указан телефон")]
+     //   [RegularExpression(@"^[+][7][(]\d{3}[)]\d{3}[-]\d{2}[-]\d{2}", ErrorMessage = "Некорректный номер телефона")]
+        public string Phone { get; set; }*/
+
+        [Required]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "ФИО")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Дата рож.")]
+        [DataType(DataType.Date)]
+        public string Birdate { get; set; }
+
+        [Required]
+        [Display(Name = "Дата рег.")]
+        [DataType(DataType.Date)]
+        public string Regdate { get; set; }
+
+        [Required]
+        [Display(Name = "Почта")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Телефон")]
+        [DataType(DataType.Text)]
         public string Phone { get; set; }
     }
 }
