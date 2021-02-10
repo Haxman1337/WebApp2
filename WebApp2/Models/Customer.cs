c225d175f555291cdb3f8d57928873c84e7dc1b1
Haxman <smartdan112233@gmail.com>
2021-02-10 11:34:10 +0300

    1

--- WebApp2/Models/Customer.cs
+++ WebApp2/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,11 +19,22 @@ namespace WebApp2.Models
             Email = email;
             Phone = phone;
         }
+        [Required]
         public string Id { get; set; }
+
+        [Required (ErrorMessage = "Не указано имя")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        [RegularExpression(@"[A-Za-zА-Яа-я\s]{3,}", ErrorMessage = "Недопустимые символы")]
         public string Name { get; set; }
+
         public string Birdate { get; set; }
+
         public string Regdate { get; set; }
+
+        [Required(ErrorMessage = "Не указана почта")]
+        [EmailAddress, ErrorMessage = "Некорректный адрес")]
         public string Email { get; set; }
+
         public string Phone { get; set; }
     }
 }
